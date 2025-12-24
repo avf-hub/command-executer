32222e44d1525814f3f676cf794387534a73afb8
Andrey Falkovskiy <avf-91@yandex.ru>
2025-12-24 18:39:37 +0300

    Работа с файлами

---
+++ src/core/files/file.service.ts
@@ -0,0 +1,26 @@
+import {dirname, isAbsolute, join} from "node:path";
+import {promises} from "node:fs";
+
+export class FileService {
+    private async isExist(path: string): Promise<boolean> {
+        try {
+            await promises.stat(path);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    public getFilePath(path: string, name: string, ext: string): string {
+        if (!isAbsolute(path)) {
+            path = join(__dirname + "/" + path);
+        }
+        return join(dirname(path) + "/" + name + "." + ext);
+    }
+
+    public async deleteFileIfExists(path: string): Promise<void> {
+        if (await this.isExist(path)) {
+            await promises.unlink(path);
+        }
+    }
+}
